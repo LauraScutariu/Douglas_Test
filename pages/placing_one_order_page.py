acde53b443734e59c27c57abd4511db86365c5ed
Laura Scutariu <138318963+LauraScutariu@users.noreply.github.com>
2024-01-17 12:28:25 +0100

    Commit

--- pages/placing_one_order_page.py
+++ pages/placing_one_order_page.py
@@ -7,7 +7,7 @@ driver = webdriver.Chrome()
 driver.get("https://www.douglas.ro/")
 
 search_box = driver.find_element(By.NAME, "//input[@type='search']")
-search_box.send_keys("item")
+search_box.send_keys("DIOR Forever No-Transfer 24h Wear Matte Foundation")
 search_box.send_keys(Keys.ENTER)
 
 driver.quit()
